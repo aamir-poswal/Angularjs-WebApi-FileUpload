0fba22ed95eced906fa45e2978bcff77f78f3b3f
aamir-poswal <aamir.pucit@gmail.com>
2015-04-27 12:18:07 +0500

    initial commit

---
+++ GAN-Developer-Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GAN_Developer_Test.MvcApplication" Language="C#" %>
---
+++ GAN-Developer-Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GAN_Developer_Test.Startup))]
+namespace GAN_Developer_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
